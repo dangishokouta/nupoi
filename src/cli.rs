6d889b2260472ca80edd09d071dfcacd195a93f5
dangishokouta <90143019+dangishokouta@users.noreply.github.com>
2022-07-18 13:19:06 +0900

    Update cli.rs

--- src/cli.rs
+++ src/cli.rs
@@ -3,23 +3,11 @@ use std::path::PathBuf;
 use std::fs;
 
 #[derive(Parser)]
-#[clap(author, version, about)]
+#[clap(
+    author, version, about
+)]
+pub struct Options { 
 
-pub struct Options {
-
-    /// Select Column Name
-    #[clap(short='c', long="column", value_name = "column-Name")]
-    pub column: Vec<String>,
-
-    /// Select Column Number
-    #[clap(short='n', long="number", value_name = "column-Number")]
-    pub number: Vec<usize>,
-
-    /// Tab delimited csv file
-    #[clap(short='t', long="tab")]
-    pub tab: bool,
-
-    /// Input CSV file
     #[clap(value_name = "File", required = true, help = "対象となるCSVファイルのパス")]
     pub file: PathBuf,
 }
